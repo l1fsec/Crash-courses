292cf322c2c40eff5598d170efef8f39b0ee6228
EmberNya <54239712+l1fsec@users.noreply.github.com>
2021-06-24 21:55:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -216,3 +216,108 @@ const todoCompleted = todos.filter(function(todo) {
 */
 
 // ---------------------------------------------------------------------------------------------- 
+
+// Conditionals
+/*
+const x = 6;
+const y = 11;
+
+if(x > 5 || y > 10)  {
+  console.log('x is more than 5 or y is more than 10');
+} 
+ else if(x > 10) {
+  console.log('x is greater than 10');
+} else {
+  console.log('x is less than 10');
+}
+*/
+
+/*
+const x = 11;
+
+const color = x > 10 ? 'red' : 'blue'; // Takhle to taky jde!
+
+console.log(color);
+*/
+
+/*
+const color = 'green';
+
+switch(color) {     //Switch
+  case 'red': 
+    console.log('color is red'); 
+    break;
+  case 'blue':
+    console.log('color is blue')
+    break;
+  default:
+    console.log('color is NOT red or blue');
+    break;
+}
+*/
+
+// ---------------------------------------------------------------------------------------------- 
+
+/*
+function addNums(num1, num2) {
+  return num1 + num2;           // Prvni metoda
+}
+console.log(addNums(5,5));
+*/
+
+/*
+const addNums = (num1 = 1, num2 = 1) => { // Druha metoda, jak to udelat
+  return num1 + num2;
+}
+
+console.log(addNums(5, 5));
+*/
+
+/*
+const addNums = (num1 = 1, num2 = 1) => { // Treti metoda, jak to udelat
+  console.log(num1 + num2);
+}
+addNums(5, 5);
+*/
+
+/*
+const addNums = (num1 = 1,num2 = 1) => num1 + num2; // Ctvrta, celkem nejrychlejsi metoda
+
+console.log(addNums(5, 5));
+*/
+
+//-----------------------------------
+
+// Object oritented
+
+//Construction function
+
+function Person(firstName, lastName, dob) {   
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.dob = new Date(dob);
+  this.getBirthYear = function() {
+    return this.dob.getFullYear();
+  }
+  this.getFullName = function() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+Person.prototype.getBirthYear = function() {
+  return this.dob.getFullYear();
+}
+
+Person.prototype.getFullName = function() {
+  return `${this.firstName} ${this.lastName}`;
+}
+
+//Instantiate object
+
+const person1 = new Person('John', 'Doe', '4-3-1980');
+const person2 = new Person('Mary', 'Smith', '3-5-1970'); 
+
+console.log(person1.getBirthYear());
+console.log(person1.getFullName());
+
+
